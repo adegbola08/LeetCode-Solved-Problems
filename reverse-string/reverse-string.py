4c6bca448f40558dd0d905122b28f97de5d6cb42
Tella Adegbola <92543868+adegbola08@users.noreply.github.com>
2022-04-27 01:20:07 -0400

    Time: 197 ms, Memory: 18.6 MB - LeetHub

--- reverse-string/reverse-string.py
+++ reverse-string/reverse-string.py
@@ -1,7 +1,6 @@
 class Solution:
     def reverseString(self, s: List[str]) -> None:
         return s.reverse()
-        
         """
         Do not return anything, modify s in-place instead.
         """

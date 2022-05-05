35dfdc08373ce82f6463e5d53401a2b555f561ff
Tella Adegbola <92543868+adegbola08@users.noreply.github.com>
2022-05-05 16:58:33 -0400

    Time: 54 ms, Memory: 13.9 MB - LeetHub

--- implement-strstr/implement-strstr.py
+++ implement-strstr/implement-strstr.py
@@ -1,18 +1,5 @@
-class Solution(object):
-    def strStr(self, haystack, needle):
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
         if needle in haystack:
-            return haystack.find(needle)
-        elif needle not in haystack:
-            return -1
-        else:
-            return 0
-        
-        
-        
-        
-        """
-        :type haystack: str
-        :type needle: str
-        :rtype: int
-        """
-        
+            return haystack.index(needle)
+        return -1

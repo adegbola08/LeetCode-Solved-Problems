99984d15cf7ab79c46ea6d6ebf2d84c8f8a1d587
Tella Adegbola <92543868+adegbola08@users.noreply.github.com>
2022-05-05 01:15:34 -0400

    Added solution - LeetHub

--- Minimum element in a sorted and rotated array - GFG/minimum-element-in-a-sorted-and-rotated-array.py
+++ Minimum element in a sorted and rotated array - GFG/minimum-element-in-a-sorted-and-rotated-array.py
@@ -2,8 +2,21 @@
 
 class Solution:
     def findMin(self, arr, n):
-        arr.sort()
-        return arr[0]
+        """arr.sort()
+        return arr[0]"""
+        
+        left = 0
+        right = n-1
+        min_val = (arr[(right+left)//2])
+        
+        while left < right:
+            possible_min = min(arr[left], arr[right])
+            min_val = min(min_val, possible_min)
+            left += 1
+            right -= 1
+            
+        return min_val
+        
 
 #{ 
 #  Driver Code Starts

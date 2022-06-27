f8c4a4fb107b18054895ca351fff6f097a56575e
Tella Adegbola <92543868+adegbola08@users.noreply.github.com>
2022-06-27 11:45:49 -0400

    Time: 1079 ms (5.67%), Space: 18.9 MB (68.09%) - LeetHub

---
+++ 648-replace-words/648-replace-words.py
@@ -0,0 +1,18 @@
+class Solution:
+    def replaceWords(self, dic: List[str], sentence: str) -> str:
+        short = len(min(dic, key=len))
+        long = len(max(dic, key=len))
+        
+        lst = sentence.split(" ")
+        
+        for i in range(len(lst)):
+            cur = short
+            while cur <= long:
+                temp = lst[i][:cur]
+                if temp in dic:
+                    lst[i] = temp
+                    break
+                cur += 1
+                
+        return " ".join(lst)
+        

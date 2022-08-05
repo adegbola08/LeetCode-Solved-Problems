e438743196f50d8ac24261006c197e10a0b93ca7
Tella Adegbola <92543868+adegbola08@users.noreply.github.com>
2022-08-05 17:03:16 -0400

    Time: 310 ms, Memory: 25.4 MB - LeetHub

--- rotate-array/rotate-array.py
+++ rotate-array/rotate-array.py
@@ -1,38 +1,16 @@
-class Solution(object):
-    def rotate(self, nums, k):
-        if k < len(nums):
-            a = len(nums) - k
-            last = nums[0:a]
-            first = nums[a:]
-            nums[0:k] , nums[k:]= first,last
-            return nums
-        for i in range(k):
-            temp = nums[-1]
-            for j in range(len(nums)-1,-1,-1):
-                nums[j] = nums[j-1]
-            nums[0] = temp
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        n = len(nums)
+        rot_pos = k % n
         
-
+        copy = nums[-rot_pos:]
+        new = copy + nums[:n-rot_pos]
         
+        for i in range(len(nums)):
+            nums[i] = new[i]
+            
         
         """
-        for i in range(1,k+1):
-            nums.insert(0,nums.pop(-1))
-            i+=1
-        """
-        
-        
-        
-        """i = 1
-        while i <= k:
-            nums.insert(0,nums[-1])
-            nums.pop(-1)
-            i+=1"""
-
-        
-        """
-        :type nums: List[int]
-        :type k: int
-        :rtype: None Do not return anything, modify nums in-place instead.
+        Do not return anything, modify nums in-place instead.
         """
         

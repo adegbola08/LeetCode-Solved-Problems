a8eddd3338be160bffb708673be68537e137e653
Tella Adegbola <92543868+adegbola08@users.noreply.github.com>
2022-08-10 23:49:40 -0400

    Time: 64 ms, Memory: 14 MB - LeetHub

--- intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.py
+++ intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.py
@@ -1,6 +1,30 @@
 class Solution:
     def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        import collections
+        
         result = []
+        
+        if len(nums1) <= len(nums2):
+            count = collections.Counter(nums2)
+            
+            for elem in nums1:
+                if elem in count and count[elem] > 0:
+                    result.append(elem)
+                    count[elem] -= 1
+                    
+        else:
+            count = collections.Counter(nums1)
+            
+            for elem in nums2:
+                if elem in count and count[elem] > 0:
+                    result.append(elem)
+                    count[elem] -= 1
+            
+        
+        return result
+    
+        
+        """result = []
         
         if len(nums1) <= len(nums2):
             
@@ -16,5 +40,5 @@ class Solution:
                     result.append(elem)
                     nums1.remove(elem)
         
-        return result
+        return result"""
         

b03791571a651ff7bd971784ff8ba55271c8e8a3
Tella Adegbola <92543868+adegbola08@users.noreply.github.com>
2022-09-01 00:41:14 -0400

    Time: 68 ms (63.47%), Space: 14.5 MB (57.09%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.py
+++ 125-valid-palindrome/125-valid-palindrome.py
@@ -5,6 +5,16 @@ class Solution:
         for ch in s:
             if ch.isalnum():
                 word += ch.lower()
-         
-        return word == word[::-1]
+        
+        l, r = 0, len(word)-1
+        
+        while l < r:
+            if word[l] != word[r]:
+                return False
+            l += 1
+            r -= 1
+        
+        return True
+        
+        """return word == word[::-1]"""
         
